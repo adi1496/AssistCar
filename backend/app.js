2b3daf7616c6980b0602d51344906828b53a4c58
Adi Matei <ady_matey@yahoo.com>
2020-10-19 12:33:34 +0300

    First Deploy

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use(cors({
 }));
 app.use(morgan('dev'));
 app.use(cookieParser());
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static('public'));
 
 
 app.use('/api/v1/pages', viewRoutes);
